8e13050d01697d9fca845198aa24b1901d427f80
Gabriel Gomes <gabriel.gomes.brasil@gmail.com>
2025-09-21 09:42:28 -0300

    feat: implementa emissão de evento para excluir tarefa

--- gabriel-gomes/todo-app/src/App.vue
+++ gabriel-gomes/todo-app/src/App.vue
@@ -2,12 +2,11 @@
 
 </style>
 
-
 <template>
   <div class="container w-75 mt-5">
     <TodoHeader />
     <TodoForm />
-    <TodoList />
+    <TodoList v-bind:tasks="tasks" @delete-task="deleteTask"  />
   </div>
 </template>
 
@@ -23,6 +22,20 @@ export default {
     TodoHeader,
     TodoForm,
     TodoList,
-  }
+  },
+
+  data() {
+    return {
+      tasks: [
+        { id: 1, text: 'Configurar o ambiente de desenvolvimento', completed: true },
+        { id: 2, text: 'Aprender o básico de Vue.js', completed: false },
+      ]
+    }
+  },
+  methods: {
+  deleteTask(taskId) {
+    console.log('App.vue (PAI) ouviu o sinal! O ID da tarefa a ser deletada é:', taskId);
+  },
+ }
 }
 </script>
