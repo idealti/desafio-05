fa3184a501e0c66471a820df48ec481c7b406e87
renansui <renanddtao@gmail.com>
2025-09-21 21:21:15 -0300

    feat: first commit renan-neves

---
+++ renan-neves/src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "./main.scss";
+import "./style.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+createApp(App).mount("#app");
